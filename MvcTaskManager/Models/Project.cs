657c890747d70ce60922b0086991465bb67f77b0
avinash konduri <avinashk@gmail.com>
2019-08-07 19:23:56 +0530

    Initial Commit with Projects API

---
+++ MvcTaskManager/Models/Project.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcTaskManager.Models
+{
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime DateOfStart { get; set; }
+        public int TeamSize { get; set; }
+    }
+
+    public class TaskManagerDbContext:DbContext
+        {
+            public DbSet<Project> Projects { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("data source=L006\\MSSQLSERVER2016; integrated security=yes;initial catalog=TaskManager");
+        }
+        }
+}
